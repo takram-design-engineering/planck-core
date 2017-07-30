f6dd04088326a16f6208fc79abd206b4da0c3ef1
Shota Matsuda <shota@takram.com>
2017-07-30 17:46:52 +0900

    added webpack test

---
+++ webpack.config.unit.js
@@ -0,0 +1,76 @@
+//
+//  Takram Confidential
+//
+//  Copyright (C) 2017-Present Shota Matsuda
+//
+//  All information contained herein is, and remains the property of Takram.
+//  The intellectual and technical concepts contained herein are proprietary to
+//  Takram and may be covered by Japan and Foreign Patents, patents in process,
+//  and are protected by trade secret or copyright law. Dissemination of this
+//  information or reproduction of this material is strictly forbidden unless
+//  prior written permission is obtained from Takram. Access to the source code
+//  contained herein is hereby forbidden to anyone except current Takram
+//  employees, managers or contractors who have executed Confidentiality and
+//  Non-disclosure agreements explicitly covering such access.
+//
+//  The copyright notice above does not evidence any actual or intended
+//  publication or disclosure of this source code, which includes information
+//  that is confidential and/or proprietary, and is a trade secret, of Takram.
+//  Any reproduction, modification, distribution, public performance, or public
+//  display of or through use of this source code without the express written
+//  consent of Takram is strictly prohibited, and in violation of applicable
+//  laws and international treaties. The receipt or possession of this source
+//  code and/or related information does not convey or imply any rights to
+//  reproduce, disclose or distribute its contents, or to manufacture, use, or
+//  sell anything that it may describe, in whole or in part.
+//
+
+const path = require('path')
+const webpack = require('webpack')
+
+const pkg = require('./package.json')
+
+module.exports = {
+  entry: [
+    path.resolve(__dirname, 'test', 'unit.js'),
+  ],
+  output: {
+    filename: 'webpack.js',
+    path: path.resolve(__dirname, 'dist', 'test', 'unit'),
+  },
+  devtool: 'source-map',
+  externals: {
+    './runner': 'runner',
+    'chai': 'chai',
+    'mocha': 'mocha',
+    'nock': 'nock',
+    'sinon': 'sinon',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                'es2015',
+                'es2016',
+                'es2017',
+                'stage-3',
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.browser': JSON.stringify(true),
+      'BUNDLER': JSON.stringify('webpack'),
+    }),
+  ],
+}
