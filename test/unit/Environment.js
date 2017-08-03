e90f64d4f83b9beabfe8d9d3f1808bc6bf09efdc
Shota Matsuda <shota@takram.com>
2017-08-04 07:10:50 +0900

    added environment test

---
+++ test/unit/Environment.js
@@ -0,0 +1,49 @@
+//
+//  The MIT License
+//
+//  Copyright (C) 2016-Present Shota Matsuda
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+//  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+//
+
+import chai from 'chai'
+import detectNode from 'detect-node'
+
+import { Environment } from '../..'
+
+const expect = chai.expect
+
+describe('Environment', () => {
+  describe('#type', () => {
+    it('returns environment name', done => {
+      if (detectNode) {
+        expect(Environment.type).equal('node')
+        done()
+      } else {
+        expect(Environment.type).equal('browser')
+        const worker = new Worker('/test/unit/data/worker')
+        worker.addEventListener('message', event => {
+          expect(event.data).equal('worker')
+          done()
+        }, false)
+        worker.postMessage('')
+      }
+    })
+  })
+})
