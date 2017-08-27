c29cacfda9bb06a9fdd01218af10d6d28ee93f77
Shota Matsuda <shota@takram.com>
2017-08-27 23:47:59 +0900

    optimization for typed arrays

--- src/Stride.js
+++ src/Stride.js
@@ -22,47 +22,54 @@
 //  DEALINGS IN THE SOFTWARE.
 //
 
+function isTypedArray(array) {
+  return (
+    array instanceof Int8Array ||
+    array instanceof Uint8Array ||
+    array instanceof Uint8ClampedArray ||
+    array instanceof Int16Array ||
+    array instanceof Uint16Array ||
+    array instanceof Int32Array ||
+    array instanceof Uint32Array ||
+    array instanceof Float32Array ||
+    array instanceof Float64Array
+  )
+}
+
+function slice(array, start, end) {
+  return array.slice(start, end)
+}
+
+function subarray(array, start, end) {
+  return array.subarray(start, end)
+}
+
 export default {
   forEach(array, stride, callback) {
-    const values = Array(stride)
-    let strideIndex = 0
-    for (let index = 0; index < array.length; ++index) {
-      const modulo = index % stride
-      values[modulo] = values[index]
-      if (modulo === stride - 1) {
-        callback(values, strideIndex)
-        strideIndex += stride
-      }
+    const func = isTypedArray(array) ? subarray : slice
+    const end = array.length - array.length % stride
+    for (let start = 0, index = 0; start < end; start += stride, ++index) {
+      callback(func(array, start, start + stride), index)
     }
   },
 
   some(array, stride, callback) {
-    const values = Array(stride)
-    let strideIndex = 0
-    for (let index = 0; index < array.length; ++index) {
-      const modulo = index % stride
-      values[modulo] = values[index]
-      if (modulo === stride - 1) {
-        if (callback(values, strideIndex)) {
-          return true
-        }
-        strideIndex += stride
+    const end = array.length - array.length % stride
+    const func = isTypedArray(array) ? subarray : slice
+    for (let start = 0, index = 0; start < end; start += stride, ++index) {
+      if (callback(func(array, start, start + stride), index)) {
+        return true
       }
     }
     return false
   },
 
   every(array, stride, callback) {
-    const values = Array(stride)
-    let strideIndex = 0
-    for (let index = 0; index < array.length; ++index) {
-      const modulo = index % stride
-      values[modulo] = values[index]
-      if (modulo === stride - 1) {
-        if (!callback(values, strideIndex)) {
-          return false
-        }
-        strideIndex += stride
+    const end = array.length - array.length % stride
+    const func = isTypedArray(array) ? subarray : slice
+    for (let start = 0, index = 0; start < end; start += stride, ++index) {
+      if (!callback(func(array, start, start + stride), index)) {
+        return false
       }
     }
     return true
@@ -70,32 +77,45 @@ export default {
 
   reduce(array, stride, callback, initial) {
     let result = initial
-    const values = Array(stride)
-    let strideIndex = 0
-    for (let index = 0; index < array.length; ++index) {
-      const modulo = index % stride
-      values[modulo] = values[index]
-      if (modulo === stride - 1) {
-        result = callback(result, values, strideIndex)
-        strideIndex += stride
-      }
+    const end = array.length - array.length % stride
+    const func = isTypedArray(array) ? subarray : slice
+    for (let start = 0, index = 0; start < end; start += stride, ++index) {
+      result = callback(result, func(array, start, start + stride), index)
     }
     return result
   },
 
-  transform(array, stride, callback) {
-    const values = Array(stride)
-    let strideIndex = 0
-    for (let index = 0; index < array.length; ++index) {
-      const modulo = index % stride
-      values[modulo] = values[index]
-      if (modulo === stride - 1) {
-        const transformed = callback(values, strideIndex)
+  set(array, stride, item) {
+    const end = array.length - array.length % stride
+    if (isTypedArray(array)) {
+      for (let start = 0; start < end; start += stride) {
+        array.set(item, start)
+      }
+    } else {
+      for (let start = 0; start < end; start += stride) {
         for (let offset = 0; offset < stride; ++offset) {
           // eslint-disable-next-line no-param-reassign
-          array[index - (stride - offset - 1)] = transformed[offset]
+          array[start + offset] = item[offset]
         }
-        strideIndex += stride
+      }
+    }
+    return array
+  },
+
+  transform(array, stride, callback) {
+    const end = array.length - array.length % stride
+    if (isTypedArray(array)) {
+      for (let start = 0, index = 0; start < end; start += stride, ++index) {
+        const item = callback(array.slice(start, start + stride), index)
+        array.set(item, start)
+      }
+    } else {
+      for (let start = 0, index = 0; start < end; start += stride, ++index) {
+        for (let offset = 0; offset < stride; ++offset) {
+          const item = callback(array.slice(start, start + stride), index)
+          // eslint-disable-next-line no-param-reassign
+          array[start + offset] = item[offset]
+        }
       }
     }
     return array
--- test/unit/Stride.js
+++ test/unit/Stride.js
@@ -32,34 +32,63 @@ const expect = chai.expect
 
 describe('Stride', () => {
   describe('#forEach', () => {
-    it('works with 2 stride of an array', () => {
-      const array = [0, 'a', 1, 'b', 2, 'c']
-      let i = 0
-      Stride.forEach(array, 2, (values, index) => {
-        const [a, b] = values
-        expect(a).equal(array[i * 2 + 0])
-        expect(b).equal(array[i * 2 + 1])
-        expect(index).equal(i)
-        ++i
-      })
-      expect(i).equal(3)
+    it('works with stride of 2', () => {
+      {
+        const array = [0, 'a', 1, 'b', 2, 'c']
+        let i = 0
+        Stride.forEach(array, 2, (values, index) => {
+          const [a, b] = values
+          expect(a).equal(array[i * 2 + 0])
+          expect(b).equal(array[i * 2 + 1])
+          expect(index).equal(i)
+          ++i
+        })
+        expect(i).equal(3)
+      }
+      {
+        const array = new Uint8Array([0, 1, 2, 3, 4, 5])
+        let i = 0
+        Stride.forEach(array, 2, (values, index) => {
+          const [a, b] = values
+          expect(a).equal(array[i * 2 + 0])
+          expect(b).equal(array[i * 2 + 1])
+          expect(index).equal(i)
+          ++i
+        })
+        expect(i).equal(3)
+      }
     })
 
-    it('works with 3 stride of an array', () => {
-      const array = [0, 'a', 'A', 1, 'b', 'B', 2, 'c', 'C']
-      let i = 0
-      Stride.forEach(array, 3, (values, index) => {
-        const [a, b, c] = values
-        expect(a).equal(array[i * 3 + 0])
-        expect(b).equal(array[i * 3 + 1])
-        expect(c).equal(array[i * 3 + 2])
-        expect(index).equal(i)
-        ++i
-      })
-      expect(i).equal(3)
+    it('works with stride of 3', () => {
+      {
+        const array = [0, 'a', 'A', 1, 'b', 'B', 2, 'c', 'C']
+        let i = 0
+        Stride.forEach(array, 3, (values, index) => {
+          const [a, b, c] = values
+          expect(a).equal(array[i * 3 + 0])
+          expect(b).equal(array[i * 3 + 1])
+          expect(c).equal(array[i * 3 + 2])
+          expect(index).equal(i)
+          ++i
+        })
+        expect(i).equal(3)
+      }
+      {
+        const array = new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7, 8])
+        let i = 0
+        Stride.forEach(array, 3, (values, index) => {
+          const [a, b, c] = values
+          expect(a).equal(array[i * 3 + 0])
+          expect(b).equal(array[i * 3 + 1])
+          expect(c).equal(array[i * 3 + 2])
+          expect(index).equal(i)
+          ++i
+        })
+        expect(i).equal(3)
+      }
     })
 
-    it('skips residue of a stride', () => {
+    it('skips residue of stride', () => {
       const array = [0, 'a', 'A', 1, 'b', 'B', 2, 'c', 'C', 3]
       let i = 0
       Stride.forEach(array, 3, (values, index) => {
@@ -71,6 +100,359 @@ describe('Stride', () => {
         ++i
       })
       expect(i).equal(3)
+    })
+  })
+
+  describe('#some', () => {
+    it('works with stride of 2', () => {
+      {
+        const array = [0, 'a', 1, 'b', 2, 'c']
+        let i = 0
+        const result = Stride.some(array, 2, (values, index) => {
+          const [a, b] = values
+          expect(a).equal(array[i * 2 + 0])
+          expect(b).equal(array[i * 2 + 1])
+          expect(index).equal(i)
+          ++i
+          if (index === 1) {
+            return true
+          }
+          return false
+        })
+        expect(i).equal(2)
+        expect(result).true
+      }
+      {
+        const array = new Uint8Array([0, 1, 2, 3, 4, 5])
+        let i = 0
+        const result = Stride.some(array, 2, (values, index) => {
+          const [a, b] = values
+          expect(a).equal(array[i * 2 + 0])
+          expect(b).equal(array[i * 2 + 1])
+          expect(index).equal(i)
+          ++i
+          if (index === 1) {
+            return true
+          }
+          return false
+        })
+        expect(i).equal(2)
+        expect(result).true
+      }
+    })
+
+    it('works with stride of 3', () => {
+      {
+        const array = [0, 'a', 'A', 1, 'b', 'B', 2, 'c', 'C']
+        let i = 0
+        const result = Stride.some(array, 3, (values, index) => {
+          const [a, b, c] = values
+          expect(a).equal(array[i * 3 + 0])
+          expect(b).equal(array[i * 3 + 1])
+          expect(c).equal(array[i * 3 + 2])
+          expect(index).equal(i)
+          ++i
+          if (index === 1) {
+            return true
+          }
+          return false
+        })
+        expect(i).equal(2)
+        expect(result).true
+      }
+      {
+        const array = new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7, 8])
+        let i = 0
+        const result = Stride.some(array, 3, (values, index) => {
+          const [a, b, c] = values
+          expect(a).equal(array[i * 3 + 0])
+          expect(b).equal(array[i * 3 + 1])
+          expect(c).equal(array[i * 3 + 2])
+          expect(index).equal(i)
+          ++i
+          if (index === 1) {
+            return true
+          }
+          return false
+        })
+        expect(i).equal(2)
+        expect(result).true
+      }
+    })
+
+    it('skips residue of stride', () => {
+      const array = [0, 'a', 'A', 1, 'b', 'B', 2, 'c', 'C', 3]
+      let i = 0
+      const result = Stride.some(array, 3, (values, index) => {
+        const [a, b, c] = values
+        expect(a).equal(array[i * 3 + 0])
+        expect(b).equal(array[i * 3 + 1])
+        expect(c).equal(array[i * 3 + 2])
+        expect(index).equal(i)
+        ++i
+        return false
+      })
+      expect(i).equal(3)
+      expect(result).false
+    })
+  })
+
+  describe('#every', () => {
+    it('works with stride of 2', () => {
+      {
+        const array = [0, 'a', 1, 'b', 2, 'c']
+        let i = 0
+        const result = Stride.every(array, 2, (values, index) => {
+          const [a, b] = values
+          expect(a).equal(array[i * 2 + 0])
+          expect(b).equal(array[i * 2 + 1])
+          expect(index).equal(i)
+          ++i
+          if (index === 1) {
+            return false
+          }
+          return true
+        })
+        expect(i).equal(2)
+        expect(result).false
+      }
+      {
+        const array = new Uint8Array([0, 1, 2, 3, 4, 5])
+        let i = 0
+        const result = Stride.every(array, 2, (values, index) => {
+          const [a, b] = values
+          expect(a).equal(array[i * 2 + 0])
+          expect(b).equal(array[i * 2 + 1])
+          expect(index).equal(i)
+          ++i
+          if (index === 1) {
+            return false
+          }
+          return true
+        })
+        expect(i).equal(2)
+        expect(result).false
+      }
+    })
+
+    it('works with stride of 3', () => {
+      {
+        const array = [0, 'a', 'A', 1, 'b', 'B', 2, 'c', 'C']
+        let i = 0
+        const result = Stride.every(array, 3, (values, index) => {
+          const [a, b, c] = values
+          expect(a).equal(array[i * 3 + 0])
+          expect(b).equal(array[i * 3 + 1])
+          expect(c).equal(array[i * 3 + 2])
+          expect(index).equal(i)
+          ++i
+          if (index === 1) {
+            return false
+          }
+          return true
+        })
+        expect(i).equal(2)
+        expect(result).false
+      }
+      {
+        const array = new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7, 8])
+        let i = 0
+        const result = Stride.every(array, 3, (values, index) => {
+          const [a, b, c] = values
+          expect(a).equal(array[i * 3 + 0])
+          expect(b).equal(array[i * 3 + 1])
+          expect(c).equal(array[i * 3 + 2])
+          expect(index).equal(i)
+          ++i
+          if (index === 1) {
+            return false
+          }
+          return true
+        })
+        expect(i).equal(2)
+        expect(result).false
+      }
+    })
+
+    it('skips residue of stride', () => {
+      const array = [0, 'a', 'A', 1, 'b', 'B', 2, 'c', 'C', 3]
+      let i = 0
+      const result = Stride.every(array, 3, (values, index) => {
+        const [a, b, c] = values
+        expect(a).equal(array[i * 3 + 0])
+        expect(b).equal(array[i * 3 + 1])
+        expect(c).equal(array[i * 3 + 2])
+        expect(index).equal(i)
+        ++i
+        return true
+      })
+      expect(i).equal(3)
+      expect(result).true
+    })
+  })
+
+  describe('#reduce', () => {
+    it('works with stride of 2', () => {
+      {
+        const array = [0, 'a', 1, 'b', 2, 'c']
+        let i = 0
+        const result = Stride.reduce(array, 2, (reduced, values, index) => {
+          const [a, b] = values
+          expect(a).equal(array[i * 2 + 0])
+          expect(b).equal(array[i * 2 + 1])
+          expect(index).equal(i)
+          ++i
+          return [...reduced, a]
+        }, [])
+        expect(i).equal(3)
+        expect(result).deep.equal([0, 1, 2])
+      }
+      {
+        const array = new Uint8Array([0, 1, 2, 3, 4, 5])
+        let i = 0
+        const result = Stride.reduce(array, 2, (reduced, values, index) => {
+          const [a, b] = values
+          expect(a).equal(array[i * 2 + 0])
+          expect(b).equal(array[i * 2 + 1])
+          expect(index).equal(i)
+          ++i
+          return [...reduced, a]
+        }, [])
+        expect(i).equal(3)
+        expect(result).deep.equal([0, 2, 4])
+      }
+    })
+
+    it('works with stride of 3', () => {
+      {
+        const array = [0, 'a', 'A', 1, 'b', 'B', 2, 'c', 'C']
+        let i = 0
+        const result = Stride.reduce(array, 3, (reduced, values, index) => {
+          const [a, b, c] = values
+          expect(a).equal(array[i * 3 + 0])
+          expect(b).equal(array[i * 3 + 1])
+          expect(c).equal(array[i * 3 + 2])
+          expect(index).equal(i)
+          ++i
+          return [...reduced, a]
+        }, [])
+        expect(i).equal(3)
+        expect(result).deep.equal([0, 1, 2])
+      }
+      {
+        const array = new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7, 8])
+        let i = 0
+        const result = Stride.reduce(array, 3, (reduced, values, index) => {
+          const [a, b, c] = values
+          expect(a).equal(array[i * 3 + 0])
+          expect(b).equal(array[i * 3 + 1])
+          expect(c).equal(array[i * 3 + 2])
+          expect(index).equal(i)
+          ++i
+          return [...reduced, a]
+        }, [])
+        expect(i).equal(3)
+        expect(result).deep.equal([0, 3, 6])
+      }
+    })
+
+    it('skips residue of stride', () => {
+      const array = [0, 'a', 'A', 1, 'b', 'B', 2, 'c', 'C', 3]
+      let i = 0
+      const result = Stride.reduce(array, 3, (reduced, values, index) => {
+        const [a, b, c] = values
+        expect(a).equal(array[i * 3 + 0])
+        expect(b).equal(array[i * 3 + 1])
+        expect(c).equal(array[i * 3 + 2])
+        expect(index).equal(i)
+        ++i
+        return [...reduced, a]
+      }, [])
+      expect(i).equal(3)
+      expect(result).deep.equal([0, 1, 2])
+    })
+  })
+
+  describe('#set', () => {
+    it('works with stride of 2', () => {
+      {
+        const array = Array(6).fill(null)
+        const item = [0, 'a']
+        const result = Stride.set(array, 2, item)
+        expect(result).deep.equal([0, 'a', 0, 'a', 0, 'a'])
+      }
+      {
+        const array = new Uint8Array(6)
+        const item = new Uint8Array([0, 1])
+        const result = Stride.set(array, 2, item)
+        expect(Array.from(result)).deep.equal([0, 1, 0, 1, 0, 1])
+      }
+    })
+
+    it('works with stride of 3', () => {
+      {
+        const array = Array(9).fill(null)
+        const item = [0, 'a', 'A']
+        const result = Stride.set(array, 3, item)
+        expect(result).deep.equal([0, 'a', 'A', 0, 'a', 'A', 0, 'a', 'A'])
+      }
+      {
+        const array = new Uint8Array(9)
+        const item = new Uint8Array([0, 1, 2])
+        const result = Stride.set(array, 3, item)
+        expect(Array.from(result)).deep.equal([0, 1, 2, 0, 1, 2, 0, 1, 2])
+      }
+    })
+
+    it('skips residue of stride', () => {
+      const array = Array(8).fill(null)
+      const item = [0, 'a', 'A']
+      const result = Stride.set(array, 3, item)
+      expect(result).deep.equal([0, 'a', 'A', 0, 'a', 'A', null, null])
+    })
+  })
+
+  describe('#transform', () => {
+    it('works with stride of 2', () => {
+      {
+        const array = [0, 1, 2, 3, 4, 5]
+        const result = Stride.transform(array, 2, (values, index) => {
+          return [values[0] + index, values[1] + index]
+        })
+        expect(result).deep.equal([0, 1, 3, 4, 6, 7])
+      }
+      {
+        const array = new Uint8Array([0, 1, 2, 3, 4, 5])
+        const result = Stride.transform(array, 2, (values, index) => {
+          return [values[0] + index, values[1] + index]
+        })
+        expect(Array.from(result)).deep.equal([0, 1, 3, 4, 6, 7])
+      }
+    })
+
+    it('works with stride of 3', () => {
+      {
+        const array = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        const result = Stride.transform(array, 3, (values, index) => {
+          return [values[0] + index, values[1] + index, values[2] + index]
+        })
+        expect(result).deep.equal([0, 1, 2, 4, 5, 6, 8, 9, 10])
+      }
+      {
+        const array = new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7, 8])
+        const result = Stride.transform(array, 3, (values, index) => {
+          return [values[0] + index, values[1] + index, values[2] + index]
+        })
+        expect(Array.from(result)).deep.equal([0, 1, 2, 4, 5, 6, 8, 9, 10])
+      }
+    })
+
+    it('skips residue of stride', () => {
+      const array = [0, 1, 2, 3, 4, 5, 6, 7]
+      const result = Stride.transform(array, 3, (values, index) => {
+        return [values[0] + index, values[1] + index, values[2] + index]
+      })
+      expect(result).deep.equal([0, 1, 2, 4, 5, 6, 6, 7])
     })
   })
 })
