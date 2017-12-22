eca855a0e8de15b6951b12d50755fb2b7671d52a
Shota Matsuda <shota@takram.com>
2017-12-22 18:26:29 +0900

    named-export all the functions for file path

--- src/FilePath.js
+++ src/FilePath.js
@@ -29,7 +29,7 @@ import External from './External'
 
 const nodePath = External.node('path')
 
-function currentScriptPath() {
+export function currentScriptPath() {
   switch (Environment.type) {
     case 'browser': {
       // eslint-disable-next-line no-underscore-dangle
@@ -47,47 +47,70 @@ function currentScriptPath() {
   return undefined
 }
 
-const initialScriptPath = currentScriptPath()
+let resolve
+let normalize
+let join
+let relative
+let dirname
+let basename
+let extname
+let delimiter
+let separator
 
-let aliases
 if (Environment.type === 'node') {
-  aliases = {
-    resolve: nodePath.resolve,
-    normalize: nodePath.normalize,
-    join: nodePath.join,
-    relative: nodePath.relative,
-    dirname: nodePath.dirname,
-    basename: nodePath.basename,
-    extname: nodePath.extname,
-    separator: nodePath.sep,
-    delimiter: nodePath.delimiter,
-  }
+  ({
+    resolve,
+    normalize,
+    join,
+    relative,
+    dirname,
+    basename,
+    extname,
+    delimiter,
+    sep: separator,
+  } = nodePath)
 } else {
-  aliases = {
-    resolve(...paths) {
-      return browserPath.resolve('/', ...paths)
-    },
-
-    normalize: browserPath.normalize,
-    join: browserPath.join,
-    relative: browserPath.relative,
-    dirname: browserPath.dirname,
-    basename: browserPath.basename,
-    extname: browserPath.extname,
-    separator: browserPath.sep,
-    delimiter: browserPath.delimiter,
+  resolve = function resolve(...paths) {
+    return browserPath.resolve('/', ...paths)
   }
+  ({
+    normalize,
+    join,
+    relative,
+    dirname,
+    basename,
+    extname,
+    delimiter,
+    sep: separator,
+  } = browserPath)
+}
+
+export {
+  resolve,
+  normalize,
+  join,
+  relative,
+  dirname,
+  basename,
+  extname,
+  delimiter,
+  separator,
 }
 
 export default {
+  resolve,
+  normalize,
+  join,
+  relative,
+  dirname,
+  basename,
+  extname,
+  delimiter,
+  separator,
+
   self: initialScriptPath,
 
   get current() {
     return currentScriptPath()
   },
-
-  // Object rest spread must be placed at the last because this will be
-  // transpiled to Object.assign with the above and aliases. Othewise current()
-  // will become a static variable, not a getter function.
-  ...aliases,
 }
