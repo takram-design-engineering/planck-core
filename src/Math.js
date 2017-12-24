aa22fd4a0261ec04f0e8025985e5e1ee99e47f5a
Shota Matsuda <shota@takram.com>
2017-12-24 22:16:05 +0900

    added clamp and mix aliases and export named functions

--- src/Math.js
+++ src/Math.js
@@ -22,51 +22,55 @@
 //  DEALINGS IN THE SOFTWARE.
 //
 
-function lerp(start, stop, amount) {
+export function lerp(start, stop, amount) {
   return start + (stop - start) * amount
 }
 
-function constrain(value, min, max) {
+export function constrain(value, min, max) {
   return (value < min ? min : (value > max ? max : value))
 }
 
-function map(value, min1, max1, min2, max2) {
+export function map(value, min1, max1, min2, max2) {
   return min2 + (max2 - min2) * ((value - min1) / (max1 - min1))
 }
 
-function wrap(value, min, max) {
+export function wrap(value, min, max) {
   if (value < min) {
     return max - ((min - value) % (max - min))
   }
   return min + ((value - min) % (max - min))
 }
 
-// GLSL functions
+// GLSL-fravored functions
 
-const RADIANS = Math.PI / 180
-const DEGREES = 180 / Math.PI
+const degreesToRadians = Math.PI / 180
+const radiansToDegrees = 180 / Math.PI
 
-function radians(degrees) {
-  return degrees * RADIANS
+export function radians(degrees) {
+  return degrees * degreesToRadians
 }
 
-function degrees(radians) {
-  return radians * DEGREES
+export function degrees(radians) {
+  return radians * radiansToDegrees
 }
 
-function fract(value) {
+export function fract(value) {
   return value - Math.floor(value)
 }
 
-function mod(value, divisor) {
+export function mod(value, divisor) {
   return value - divisor * Math.floor(value / divisor)
 }
 
-function step(edge, value) {
+export const clamp = constrain
+
+export const mix = lerp
+
+export function step(edge, value) {
   return value < edge ? 0 : 1
 }
 
-function smoothstep(edge0, edge1, value) {
+export function smoothstep(edge0, edge1, value) {
   const t = constrain((value - edge0) / (edge1 - edge0), 0, 1)
   return t * t * (3 - 2 * t)
 }
@@ -80,6 +84,8 @@ export default {
   degrees,
   fract,
   mod,
+  clamp,
+  mix,
   step,
   smoothstep,
 }
