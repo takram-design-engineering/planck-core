0d521c61a76f9a31222d806a8d8fef5b65df03ce
Shota Matsuda <shota@takram.com>
2018-04-18 12:27:56 +0900

    bundle and transpile the module target

---
+++ src/main.js
@@ -0,0 +1,57 @@
+// The MIT License
+// Copyright (C) 2016-Present Shota Matsuda
+
+import Aggregate from './Aggregate'
+import AggregateFunction from './AggregateFunction'
+import Array from './Array'
+import AssertionError from './AssertionError'
+import External from './External'
+import FilePath from './FilePath'
+import Global from './Global'
+import Hash from './Hash'
+import ImplementationError from './ImplementationError'
+import Math from './Math'
+import Namespace from './Namespace'
+import Request from './Request'
+import Semaphore from './Semaphore'
+import Stride from './Stride'
+import URL from './URL'
+
+export * from './External'
+export * from './Global'
+
+export {
+  Aggregate,
+  AggregateFunction,
+  Array,
+  AssertionError,
+  External,
+  FilePath,
+  Global,
+  Hash,
+  ImplementationError,
+  Math,
+  Namespace,
+  Request,
+  Semaphore,
+  Stride,
+  URL
+}
+
+export default {
+  Aggregate,
+  AggregateFunction,
+  Array,
+  AssertionError,
+  External,
+  FilePath,
+  Global,
+  Hash,
+  ImplementationError,
+  Math,
+  Namespace,
+  Request,
+  Semaphore,
+  Stride,
+  URL
+}
