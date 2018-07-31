7c2d3a223f047d6b9b3757d5faba5aad1cf0d5c5
Shota Matsuda <shota@takram.com>
2018-07-31 19:34:54 +0900

    Add further tests for semaphore

--- test/src/Semaphore.js
+++ test/src/Semaphore.js
@@ -2,10 +2,12 @@
 // Copyright (C) 2016-Present Shota Matsuda
 
 import chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
 
 import { Semaphore } from '../..'
 
 const { expect } = chai
+chai.use(chaiAsPromised)
 
 describe('Semaphore', () => {
   it('runs tasks below or equal its capacity', () => {
@@ -23,5 +25,14 @@ describe('Semaphore', () => {
     for (let i = 0; i < 1000; ++i) {
       semaphore.wait(callback)
     }
+  })
+
+  it('rejects if callback function throws error', () => {
+    const semaphore = new Semaphore(10)
+    const error = new Error()
+    return expect(semaphore.wait(() => { throw error }))
+      .rejected.then(result => {
+        expect(result).equal(error)
+      })
   })
 })
