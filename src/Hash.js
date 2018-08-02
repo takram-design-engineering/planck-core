6a0c59e83cb34126a57da218b93604c3d8a7a32b
Shota Matsuda <shota@takram.com>
2018-08-02 13:42:22 +0900

    Switch the hash function from MD5 to SHA-256

--- src/Hash.js
+++ src/Hash.js
@@ -1,11 +1,11 @@
 // The MIT License
 // Copyright (C) 2016-Present Shota Matsuda
 
-import md5 from 'md5'
+import sha256 from 'hash.js/lib/hash/sha/256'
 import stringify from 'json-stable-stringify'
 
 export function generateHash (object) {
-  return md5(stringify(object))
+  return sha256().update(stringify(object)).digest('hex')
 }
 
 export default generateHash
